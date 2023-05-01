3398bb6c571d27f5fd3f468950455a957558a3bf
equationzhao <equationzhao@foxmail.com>
2023-05-01 14:55:05 +0800

    use maphash as default hasher

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Equationzhao/tsmap
 
 go 1.20
+
+require github.com/dolthub/maphash v0.1.0
--- tsmap.go
+++ tsmap.go
@@ -1,74 +1,50 @@
 package tsmap
 
 import (
-	"fmt"
 	"sync"
+
+	"github.com/dolthub/maphash"
 )
 
 type Shard[k comparable, v any] struct {
-	Lock        sync.RWMutex
-	InternalMap map[k]*v
+	lock        sync.RWMutex
+	InternalMap map[k]v
+}
+
+func (s *Shard[k, v]) Lock() {
+	s.lock.Lock()
+}
+
+func (s *Shard[k, v]) Unlock() {
+	s.lock.Unlock()
+}
+
+func (s *Shard[k, v]) RLock() {
+	s.lock.RLock()
+}
+
+func (s *Shard[k, v]) RUnlock() {
+	s.lock.RUnlock()
 }
 
 type Map[k comparable, v any] struct {
 	m      []*Shard[k, v]
 	shards int
-	Hash   func(k) uint32
+	Hash   func(k) uint64
 }
 
 func (p *Map[k, v]) Load(key k) (value v, ok bool) {
-	return p.GetCopy(key)
+	return p.Get(key)
 }
 
 func (p *Map[k, v]) Store(key k, value v) {
-	p.SetCopy(key, value)
+	p.Set(key, value)
 }
 
-func DefaultHash[k comparable](i k) uint32 {
-	var hash uint32 = 5381 // magic constant, apparently this hash fewest collisions possible.
-	data := fmt.Sprint(i)
-	for _, c := range data {
-		hash = ((hash << 5) + hash) + uint32(c)
-	}
-	return hash
-}
-
-type integer interface {
-	int | uint | int16 | uint16 | int32 | uint32 | int64 | uint64
-}
-
-func DefaultIntHash[k integer](key k) uint32 {
-	key = ^key + (key << 15)
-	key = (key << 15) - key - 1
-	key = key ^ (key >> 12)
-	key = key + (key << 2)
-	key = key ^ (key >> 4)
-	key = key * k(2057)
-	key = (key + (key << 3)) + (key << 11)
-	key = key ^ (key >> 16)
-	return uint32(key)
-}
-
-func DefaultStringHash(s string) uint32 {
-	var hash uint32 = 5381
-	for _, c := range s {
-		hash = ((hash << 5) + hash) + uint32(c)
-	}
-	return hash
-}
-
-func DefaultBytesHash(s []byte) uint32 {
-	var hash uint32 = 5381
-	for _, c := range s {
-		hash = ((hash << 5) + hash) + uint32(c)
-	}
-	return hash
-}
-
-const limit = 200
+var GoLimit = 70
 
 func (p *Map[k, v]) Free() {
-	if p.shards > limit {
+	if p.shards > GoLimit {
 		p.goFree()
 	} else {
 		p.free()
@@ -77,9 +53,9 @@ func (p *Map[k, v]) Free() {
 
 func (p *Map[k, v]) free() {
 	for _, m := range p.m {
-		m.Lock.Lock()
-		m.InternalMap = make(map[k]*v)
-		m.Lock.Unlock()
+		m.Lock()
+		m.InternalMap = make(map[k]v)
+		m.Unlock()
 	}
 }
 
@@ -89,9 +65,9 @@ func (p *Map[k, v]) goFree() {
 	for _, m := range p.m {
 		m := m
 		go func() {
-			m.Lock.Lock()
-			m.InternalMap = make(map[k]*v)
-			m.Lock.Unlock()
+			m.Lock()
+			m.InternalMap = make(map[k]v)
+			m.Unlock()
 			wg.Done()
 		}()
 	}
@@ -99,6 +75,25 @@ func (p *Map[k, v]) goFree() {
 }
 
 func (p *Map[k, v]) Keys() []k {
+	if p.shards < GoLimit {
+		return p.keys()
+	}
+	return p.goKeys()
+}
+
+func (p *Map[k, v]) keys() []k {
+	keys := make([]k, 0, len(p.m))
+	for _, s := range p.m {
+		s.RLock()
+		for key := range s.InternalMap {
+			keys = append(keys, key)
+		}
+		s.RUnlock()
+	}
+	return keys
+}
+
+func (p *Map[k, v]) goKeys() []k {
 	mu := new(sync.Mutex)
 	length := 0
 	for i := range p.m {
@@ -110,7 +105,7 @@ func (p *Map[k, v]) Keys() []k {
 	for _, m := range p.m {
 		m := m
 		go func() {
-			m.Lock.RLock()
+			m.RLock()
 			defer wg.Done()
 			keysi := make([]k, 0, len(m.InternalMap))
 			for k := range m.InternalMap {
@@ -119,134 +114,83 @@ func (p *Map[k, v]) Keys() []k {
 			mu.Lock()
 			keys = append(keys, keysi...)
 			mu.Unlock()
-			m.Lock.RUnlock()
+			m.RUnlock()
 		}()
 	}
 	wg.Wait()
 	return keys
 }
 
-func (p *Map[k, v]) SetCopy(key k, value v) {
-	i := p.Hash(key) & uint32(p.shards-1)
-	p.m[i].Lock.Lock()
-	defer p.m[i].Lock.Unlock()
-	p.m[i].InternalMap[key] = &value
+func (p *Map[k, v]) Set(key k, value v) {
+	shard := p.getShard(key)
+	shard.Lock()
+	defer shard.Unlock()
+	shard.InternalMap[key] = value
 }
 
-func (p *Map[k, v]) Set(key k, valuePtr *v) {
-	i := p.Hash(key) & uint32(p.shards-1)
-	p.m[i].Lock.Lock()
-	defer p.m[i].Lock.Unlock()
-	p.m[i].InternalMap[key] = valuePtr
-}
-
-func (p *Map[k, v]) Get(key k) (valuePtr *v, ok bool) {
-	i := p.Hash(key) & uint32(p.shards-1)
-	p.m[i].Lock.RLock()
-	defer p.m[i].Lock.RUnlock()
-	valuePtr, ok = p.m[i].InternalMap[key]
+func (p *Map[k, v]) Get(key k) (value v, ok bool) {
+	shard := p.getShard(key)
+	shard.RLock()
+	defer shard.RUnlock()
+	value, ok = shard.InternalMap[key]
 	return
-}
-
-func (p *Map[k, v]) GetCopy(key k) (value v, ok bool) {
-	i := p.Hash(key) & uint32(p.shards-1)
-	p.m[i].Lock.RLock()
-	defer p.m[i].Lock.RUnlock()
-	valuePtr, ok := p.m[i].InternalMap[key]
-	if ok {
-		return *valuePtr, ok
-	} else {
-		return value, ok
-	}
 }
 
 // GetOrInit return the value itself(pointer)
 // if already exists, return value and false
 // if not, use init func and store the new value, return value and true
-func (p *Map[k, v]) GetOrInit(key k, init func() *v) (actual *v, initialized bool) {
-	i := p.Hash(key) & uint32(p.shards-1)
-	p.m[i].Lock.RLock()
-	valuePtr, ok := p.m[i].InternalMap[key]
-	p.m[i].Lock.RUnlock()
+func (p *Map[k, v]) GetOrInit(key k, init func() v) (actual v, initialized bool) {
+	shard := p.getShard(key)
+	shard.RLock()
+	value, ok := shard.InternalMap[key]
+	shard.RUnlock()
 	if ok {
 		// load
-		return valuePtr, true
+		return value, true
 	}
 
-	p.m[i].Lock.Lock()
-	actual, ok = p.m[i].InternalMap[key]
+	shard.Lock()
+	actual, ok = shard.InternalMap[key]
 	if !ok {
 		// init
 		actual = init()
-		p.m[i].InternalMap[key] = actual
+		shard.InternalMap[key] = actual
 	}
-	p.m[i].Lock.Unlock()
+	shard.Unlock()
 	return
 }
 
-// GetCopyOrInit return the value copy
-// if already exists, return value copy and false
-// if not, use init func and store the new value, return value copy and true
-func (p *Map[k, v]) GetCopyOrInit(key k, init func() *v) (actual v, initialized bool) {
-	i := p.Hash(key) & uint32(p.shards-1)
-	p.m[i].Lock.RLock()
-	actualPtr, ok := p.m[i].InternalMap[key]
-	p.m[i].Lock.RUnlock()
-	if ok {
-		// load
-		return *actualPtr, true
-	}
-
-	p.m[i].Lock.Lock()
-	actualPtr, ok = p.m[i].InternalMap[key]
-	if !ok {
-		// init
-		actualPtr = init()
-		p.m[i].InternalMap[key] = actualPtr
-	}
-	p.m[i].Lock.Unlock()
-	return *actualPtr, false
+func (p *Map[k, v]) getShard(key k) *Shard[k, v] {
+	i := p.Hash(key) & uint64(p.shards-1)
+	return p.m[i]
 }
 
 // LoadOrStore returns the existing value for the key if present.
 // Otherwise, it stores and returns the given value.
 // The loaded result is true if the value was loaded, false if stored.
 func (p *Map[k, v]) LoadOrStore(key k, value v) (actual v, loaded bool) {
-	i := p.Hash(key) & uint32(p.shards-1)
-	p.m[i].Lock.RLock()
-	valuePtr, ok := p.m[i].InternalMap[key]
-	p.m[i].Lock.RUnlock()
-	if ok {
-		// load
-		return *valuePtr, true
-	}
-
-	p.m[i].Lock.Lock()
-	valuePtr, ok = p.m[i].InternalMap[key]
-	if ok {
-		// load
-		p.m[i].Lock.Unlock()
-		return *valuePtr, true
-	} else {
-		// store
-		p.m[i].InternalMap[key] = &value
-		p.m[i].Lock.Unlock()
+	value, initialized := p.GetOrInit(key, func() v {
+		return value
+	})
+	if initialized {
 		return value, false
 	}
+	return value, true
 }
 
 func NewTSMap[k comparable, v any](len int) *Map[k, v] {
+	hasher := maphash.NewHasher[k]()
 	m := &Map[k, v]{
 		m:      make([]*Shard[k, v], len),
 		shards: len,
-		Hash:   DefaultHash[k],
+		Hash:   hasher.Hash,
 	}
 
 	wg := sync.WaitGroup{}
 	wg.Add(len)
 	for i := 0; i < len; i++ {
 		go func(i int) {
-			m.m[i] = &Shard[k, v]{InternalMap: make(map[k]*v)}
+			m.m[i] = &Shard[k, v]{InternalMap: make(map[k]v)}
 			wg.Done()
 		}(i)
 	}
